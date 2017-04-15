92eb1464ea7f5396726199b11af1382d9198f480
Luke Bayes <lukebayes@gmail.com>
2017-04-14 23:45:23 -0400

    Fixed formatting to match original lint rules

--- src/create_event.js
+++ src/create_event.js
@@ -2,39 +2,39 @@ import { defaults, eventGroupByType, initialisersByGroup, initialiserParams } fr
 import extendWithKeyboardParams from './utils/extendWithKeyboardParams.js';
 
 function createEvent ( window, type, params = {} ) {
-  let group = eventGroupByType[ type ];
-  let isKeyboardEvent;
+	let group = eventGroupByType[ type ];
+	let isKeyboardEvent;
 
-  if ( group === 'KeyboardEvent' ) {
-    group = 'Event'; // because you can't fake KeyboardEvents well in any browser
-    isKeyboardEvent = true;
-  }
+	if ( group === 'KeyboardEvent' ) {
+		group = 'Event'; // because you can't fake KeyboardEvents well in any browser
+		isKeyboardEvent = true;
+	}
 
-  const initialiser = ( initialisersByGroup[ group ] || initialisersByGroup.Event );
+	const initialiser = ( initialisersByGroup[ group ] || initialisersByGroup.Event );
 
-  const Constructor = initialiser[0] || window.Event;
-  const method = initialiser[1];
+	const Constructor = initialiser[0] || window.Event;
+	const method = initialiser[1];
 
-  let extendedParams = {
-    bubbles: true, // TODO some events don't bubble?
-    cancelable: true
-  };
+	let extendedParams = {
+		bubbles: true, // TODO some events don't bubble?
+		cancelable: true
+	};
 
-  const paramsList = initialiserParams[ method ];
-  let i = ( paramsList ? paramsList.length : 0 );
+	const paramsList = initialiserParams[ method ];
+	let i = ( paramsList ? paramsList.length : 0 );
 
-  while ( i-- ) {
-    const paramName = paramsList[i];
-    extendedParams[ paramName ] = ( paramName in params ? params[ paramName ] : defaults[ paramName ] );
-  }
+	while ( i-- ) {
+		const paramName = paramsList[i];
+		extendedParams[ paramName ] = ( paramName in params ? params[ paramName ] : defaults[ paramName ] );
+	}
 
-  let event = new Constructor( type, extendedParams );
+	let event = new Constructor( type, extendedParams );
 
-  if ( isKeyboardEvent ) {
-    extendWithKeyboardParams( event, params );
-  }
+	if ( isKeyboardEvent ) {
+		extendWithKeyboardParams( event, params );
+	}
 
-  return event;
+	return event;
 }
 
 createEvent.mode = 'modern';
--- src/simulant.js
+++ src/simulant.js
@@ -1,14 +1,14 @@
 import createEvent from './create_event.js';
 
 function simulant(window, type, params = {}) {
-  return createEvent(window, type, params);
+	return createEvent(window, type, params);
 }
 
 simulant.fire = function(element, typeOrEvent, params) {
-  const event = typeof typeOrEvent === 'string' ? createEvent(element && element.ownerDocument && element.ownerDocument.defaultView, typeOrEvent, params) : typeOrEvent;
+	const event = typeof typeOrEvent === 'string' ? createEvent(element && element.ownerDocument && element.ownerDocument.defaultView, typeOrEvent, params) : typeOrEvent;
 
-  element.dispatchEvent(typeOrEvent);
-  return typeOrEvent;
+	element.dispatchEvent(event);
+	return event;
 };
 
 export default simulant;
